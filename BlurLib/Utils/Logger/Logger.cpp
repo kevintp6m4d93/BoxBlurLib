b8928f69e4119b44356a33056b37bae34a187468
clt\KevinYK_Chen <kevinyk_chen@cyberlink.com>
2026-01-05 17:55:22 +0800

    (blur project) implemented logger class

---
+++ BlurLib/Utils/Logger/Logger.cpp
@@ -0,0 +1,58 @@
+#include "Logger.h"
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+using logging::Logger;
+using logging::LogLevel;
+
+std::string Logger::levelToString(LogLevel level) const {
+	switch (level) {
+	case LogLevel::DEBUG: return "DEBUG";
+	case LogLevel::INFO: return "INFO";
+	case LogLevel::WARNING: return "WARNING";
+	case LogLevel::ERR: return "ERROR";
+	default: return "UNKNOWN";
+	}
+}
+
+std::string Logger::getCurrentTimestamp() const {
+	auto now = std::chrono::system_clock::now();
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+		now.time_since_epoch()
+	).count();
+	
+	std::ostringstream oss;
+	oss << ms;
+	return oss.str();
+}
+
+void Logger::SetLogFile(const std::string& filename) {
+	std::lock_guard<std::mutex> lock(logMutex);
+	if (logFile.is_open()) {
+		logFile.close();
+	}
+	logFile.open(filename, std::ios::app);
+}
+
+void Logger::Log(LogLevel level, const std::string& message, const std::string& filename, const int line) {
+#ifdef DISABLE_LOGGING
+	return;
+#endif
+	if (level < minLogLevel) {
+		return;
+	}
+	std::string currentTimestamp = getCurrentTimestamp();
+	std::lock_guard<std::mutex> lock(logMutex);
+	std::stringstream ss;
+	ss << "[" << currentTimestamp << "] "
+		<< "[" << levelToString(level) << "] "
+		<< "[" << filename << ":" << line << "] "
+		<< message << std::endl;
+	std::string formattedMessage = ss.str();
+
+	std::cout << formattedMessage;
+	if (logFile.is_open()) {
+		logFile << formattedMessage;
+		logFile.flush();
+	}
+}
---
+++ BlurLib/Utils/Logger/Logger.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <mutex>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+namespace logging {
+	enum class LogLevel {
+		DEBUG = 0,
+		INFO = 1,
+		WARNING = 2,
+		ERR = 3
+	};
+
+	class Logger {
+	public:
+		static Logger& GetInstance() {
+			static Logger instance;
+			return instance;
+		}
+		void SetMinLogLevel(LogLevel level) {
+			minLogLevel = level;
+		}
+		void Log(LogLevel level, const std::string& message, const std::string& filename, const int line);
+		void SetLogFile(const std::string& filename);
+
+	private:
+		std::ofstream logFile;
+		std::mutex logMutex;
+		LogLevel minLogLevel;
+
+		std::string getCurrentTimestamp() const;
+		std::string levelToString(LogLevel level) const;
+		Logger() : minLogLevel(LogLevel::DEBUG) {}
+		~Logger() {
+			logFile.close();
+		}
+		Logger(const Logger&) = delete;
+		void operator=(const Logger&) = delete;
+	};
+}
+
+#ifdef DISABLE_LOGGING
+#define LOG_INTERNAL(logLevel, msg, file, line) ((void)0)
+#else
+#define LOG_INTERNAL(logLevel, msg, file, line) logging::Logger::GetInstance().Log(logLevel, msg, file, line)
+#endif
+
+#define LOG(logLevel, msg) LOG_INTERNAL(logLevel, msg, __FILE__, __LINE__)
+#define LOG_DEBUG(msg) LOG_INTERNAL(logging::LogLevel::DEBUG, msg, __FILE__, __LINE__)
+#define LOG_INFO(msg) LOG_INTERNAL(logging::LogLevel::INFO, msg, __FILE__, __LINE__)
+#define LOG_WARNING(msg) LOG_INTERNAL(logging::LogLevel::WARNING, msg, __FILE__, __LINE__)
+#define LOG_ERROR(msg) LOG_INTERNAL(logging::LogLevel::ERR, msg, __FILE__, __LINE__)
